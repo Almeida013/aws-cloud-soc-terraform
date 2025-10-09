0fb8f47f8a2243d3d9adcd13b8c490ab14c9445b
Almeida013 <kaike-am@hotmail.com>
2025-10-09 17:08:58 -0300

    feat: Adiciona IGW e Route Table para conectar a VPC à internet

--- main.tf
+++ main.tf
@@ -14,3 +14,35 @@ resource "aws_subnet" "public_subnet" {
     Name = "soc-project-public-subnet"
   }
 }
+
+
+# 1. Cria o Internet Gateway para dar acesso à internet para a VPC
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "soc-project-igw"
+  }
+}
+
+# 2. Cria uma nova Tabela de Rotas para a sub-rede pública
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    # Para todo o tráfego destinado à internet (0.0.0.0/0),
+    # envie através do nosso Internet Gateway.
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+
+  tags = {
+    Name = "soc-project-public-rt"
+  }
+}
+
+# 3. Associa a nova Tabela de Rotas com a nossa sub-rede pública
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
